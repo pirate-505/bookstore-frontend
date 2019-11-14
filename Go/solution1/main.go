6a49e227f8030391ada1834735e9552ed391380e
Ruslan Timofieiev <rtim0905@gmail.com>
2019-11-14 23:54:47 +0200

    fix unicode currency symbols

--- Go/solution1/main.go
+++ Go/solution1/main.go
@@ -5,41 +5,48 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 )
 
 type bookPrice struct {
 	money    uint
-	currency string
+	currency rune
 }
 
-func printTotal(prices []bookPrice) {
-	var total uint
-	for _, price := range prices {
-		total += price.money
-	}
-	fmt.Printf("To pay: %d%s\n", total, prices[len(prices)-1].currency)
-}
-
-func parseBooks() ([]bookPrice, error) {
-	books := make([]bookPrice, len(os.Args)-1)
-	for _, arg := range os.Args[1:] {
-		cost, err := strconv.Atoi(arg[:len(arg)-1])
+func parseBooks(args []string) ([]bookPrice, error) {
+	// books := make([]bookPrice, len(args)-1)
+	var books []bookPrice
+	for _, arg := range args[1:] {
+		currency, _ := utf8.DecodeLastRuneInString(arg)
+		arg = strings.TrimSuffix(arg, string(currency))
+		cost, err := strconv.Atoi(arg[:len(arg)])
 		if err != nil {
 			return nil, err
 		}
+
 		book := bookPrice{
 			money:    uint(cost),
-			currency: string(arg[len(arg)-1]),
+			currency: currency,
 		}
 		books = append(books, book)
 	}
 	return books, nil
 }
 
+func printTotal(prices []bookPrice) (uint, rune) {
+	var total uint
+	for _, price := range prices {
+		total += price.money
+	}
+	return total, prices[0].currency
+}
+
 func main() {
-	prices, err := parseBooks()
+	prices, err := parseBooks(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
-	printTotal(prices)
+	total, currency := printTotal(prices)
+	fmt.Printf("To pay: %d%v\n", total, string(currency))
 }
