20b645be1ee118d98c41aa4afb3d9ca07a2c6017
juliancoffee <lightdarkdaughter@gmail.com>
2019-11-15 09:10:07 +0200

    Fix

--- Racket/solution1/bookstore.rkt
+++ Racket/solution1/bookstore.rkt
@@ -1,9 +1,7 @@
 #lang racket
 
-(provide (all-defined-out))
 (struct price 
-  (value currency)
-  #:transparent) 
+  (value currency)) 
 
 ;; parse-book : string -> book
 ;; turn string like 8$ into price with value and currency
@@ -20,7 +18,7 @@
       [else 
         (parse-try 
           (substring price-str 0 (- pivot 1))
-          (substring price-str (- pivot 1) pivot)
+          (substring price-str (- pivot 1) len)
           (- pivot 1))]))
   (parse-try price-str "" len))
 
@@ -31,15 +29,20 @@
         (parse-price arg))
       (vector->list (current-command-line-arguments))))
 
-(define result-price
+(define (result-price)
   (price 
     (foldl + 0 (map price-value list-of-prices))
-    "$"))
+    (price-currency (car list-of-prices))))
 
-(if (< (vector-length (current-command-line-arguments)) 2)
-  (and
-    (display "usage: racket bookstore.rkt <cost1>[currency] [cost2 cost3 ...]\nExample: racket bookstore.rkt 9$ 9$\n")
-    (exit))
-    (display (format "~a~a\n" 
-                 (price-value result-price)
-                 (price-currency result-price))))
+(define usage
+  "usage: racket bookstore.rkt <cost1>[currency] [cost2 cost3 ...]\nExample: racket bookstore.rkt 9$ 9$\n")
+
+(cond [(< (vector-length (current-command-line-arguments)) 2)
+       (display usage)]
+      [(> (length (remove-duplicates (map price-currency list-of-prices))) 1)
+       (display (string-append "Currency must be the same\n"
+                               usage))] 
+      [else  
+        (display (format "~a~a\n" 
+                         (price-value (result-price))
+                         (price-currency (result-price))))])
